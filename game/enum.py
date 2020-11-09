2bf6827d89c24c9ecad709c29e9e5132107dcba4
Jaeseong98 <gossip6017@gamil.com>
2020-11-10 00:01:08 +0900

    [game] Fix enum name

--- game/enum.py
+++ game/enum.py
@@ -1,12 +1,19 @@
 import enum
 
-class PointState(enum.Enum):
+class GameMode(enum.Enum):
+    HUMAN_HUMAN = "HUMAN_HUMAN"
+    HUMAN_COMPUTER = "HUMAN_COMPUTER"
+    COMPUTER_HUMAN = "COMPUTER_HUMAN"
+    COMPUTER_COMPUTER = "COMPUTER_COMPUTER"
+
+
+class PointStateEnum(enum.Enum):
     BLANK = 0
     UNSELECTABLE = 1
     BLACK = 2
     WHITE = 3
 
 
-class TurnState(enum.Enum):
-    BLANK = 0
+class TurnStateEnum(enum.Enum):
+    BLACK = 0
     WHITE = 1
