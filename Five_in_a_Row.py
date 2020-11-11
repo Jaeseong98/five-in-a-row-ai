cca92a536d9ffeb854aeebd31547998d46fec9d8
minsang <minsang5749@naver.com>
2020-11-12 02:35:06 +0900

    [GAME] Refactoring codes

--- Five_in_a_Row.py
+++ Five_in_a_Row.py
@@ -62,7 +62,6 @@ def check_unselectable_rules(originPoint, point):
         count = count1 + count2 + 1
         isOpen = isOpen1 and isOpen2
 
-        #print("Result: " + str(point) + " " + str(((count1, count2), (isOpen1, isOpen2))) + " " + str((count, isOpen)))
         if (count == 3 and isOpen):
             count33 += 1
         
@@ -77,8 +76,8 @@ def check_unselectable_rules(originPoint, point):
             break
     return
 
-def check_point_condition(point, different, is_blank_include = False, blank_count = 0):
-    point = (point[0] + different[0], point[1] + different[1])
+def check_point_condition(point, direction, is_blank_include = False, blank_count = 0):
+    point = (point[0] + direction[0], point[1] + direction[1])
     row, col = point
     
     #print("Start Check Point Function " + str(row)  + " " + str(col))
@@ -90,24 +89,48 @@ def check_point_condition(point, different, is_blank_include = False, blank_coun
         if is_blank_include:
             return (0, True)
         else:
-            _count, _isOpen = check_point_condition(point, different, is_blank_include, blank_count + 1)
+            _count, _isOpen = check_point_condition(point, direction, is_blank_include, blank_count + 1)
             return (_count, True and _isOpen)
     else:
         if blank_count > 0:
             is_blank_include = True
-        _count, _isOpen = check_point_condition(point, different, is_blank_include, blank_count)
+        _count, _isOpen = check_point_condition(point, direction, is_blank_include, blank_count)
         return (_count + 1, True and _isOpen)
 
 def is_out_of_array(point):
     row, col = point
     return (row < 0 or 14 < row) or (col < 0 or 14 < col)
 
-def is_finished_game(leftSelectableCount, point):
+def is_finished_game(leftSelectableCount, point, isWhiteTurn):
     if leftSelectableCount == 0:
         return True
     else:
-        # Find Line (point)
-        return False
+        return check_lines(point, isWhiteTurn)
+
+def check_lines(point, isWhiteTurn):
+    row, col = point
+    directionList = [((1, 0), (-1, 0)), ((0, 1), (0, -1)), ((1, 1), (-1, -1)), ((1, -1), (-1, 1))]
+
+    for direction in directionList:
+        count1 = check_line_recursion(point, direction[0])
+        count2 = check_line_recursion(point, direction[1])
+        count = count1 + count2 + 1
+
+        if count == 5 and isWhiteTurn == False:
+            return True
+        elif count >= 5:
+            return True
+    return False
+
+def check_line_recursion(point, direction):
+    point = (point[0] + direction[0], point[1] + direction[1])
+    row, col = point
+
+    if is_out_of_array(point) or array[row][col] != 2:
+        return 0
+    else:
+        return 1 + check_lines(point, direction)
+
 
 # Main Logic
 array = [ [ 0 for i in range(15) ] for j in range(15)]
@@ -116,14 +139,16 @@ for element in array:
      
 isWhiteTurn = False
 leftSelectableCount = 225
+point = (0, 0)
 
+#while is_finished_game(leftSelectableCount, point, isWhiteTurn):
 while True:
     isCorrect = False
     while isCorrect == False:
         try:
-            row, col = input("Input(row, col): ").split()
-            row = int(row)
-            col = int(col)
+            point = input("Input(row, col): ").split()
+            row = int(point[0])
+            col = int(point[1])
             if not(0 <=row and row <= 14) and not(0 <= col and col <= 14):
                 raise OutOfIndexError
             if array[row][col] == 2 or array[row][col] == 3:
