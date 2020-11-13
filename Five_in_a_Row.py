38168d9602e0c0e60e91722dd81c726c5097111d
minsang <minsang5749@naver.com>
2020-11-13 20:36:06 +0900

    [GAME] Adjust over-5 rule

--- Five_in_a_Row.py
+++ Five_in_a_Row.py
@@ -76,16 +76,16 @@ def check_point_condition(point, direction, is_blank_include = False, blank_coun
         return (_count + 1, True and _isOpen)
 '''
 
-def detect_unselectable_points(point):
-    row, col = point
+def detect_unselectable_points():
     for row in range(ARRAY_SIZE):
         for col in range(ARRAY_SIZE):
             if array[row][col] == 0:
-                originPoint = (row, col)
-                check_33_rules(originPoint, originPoint)
-                check_44_rules(originPoint, originPoint)
+                point = (row, col)
+                check_33_rule(point, point)
+                check_44_rule(point, point)
+                check_over_5_rule(point)
                 if array[row][col] == 0:
-                    detect_unselectable_points_from_origin_point(originPoint)
+                    detect_unselectable_points_from_origin_point(point)
     return
 
 def detect_unselectable_points_from_origin_point(originPoint):
@@ -98,24 +98,25 @@ def detect_unselectable_points_from_origin_point(originPoint):
         row, col = point
         while is_out_of_array(point) == False and array[row][col] != 3:
             if array[row][col] == 2:
-                check_33_rules(originPoint, point)
-                check_44_rules(originPoint, point)
+                check_33_rule(originPoint, point)
+                check_44_rule(originPoint, point)
             point = (row + direction[0], col + direction[1])
             row, col = point
             if array[originRow][originCol] == 1:
                 return
+    print("Enter 0" + str(originPoint))
     array[originRow][originCol] = 0
 
-def check_33_rules(originPoint, point):
+def check_33_rule(originPoint, point):
     global unselectablePointList
 
     row, col = point
-    directionList = [((1, 0), (-1, 0)), ((0, 1), (0, -1)), ((1, 1), (-1, -1)), ((1, -1), (-1, 1))]
+    directionTupleList = [((1, 0), (-1, 0)), ((0, 1), (0, -1)), ((1, 1), (-1, -1)), ((1, -1), (-1, 1))]
     
     lineCount = 0
-    for direction in directionList:
-        count1, isOpen1, is_blank_include1 = check_discountinuous_line_recursion(point, direction[0])
-        count2, isOpen2, is_blank_include2 = check_discountinuous_line_recursion(point, direction[1], is_blank_include1)
+    for direction in directionTupleList:
+        count1, isOpen1, is_blank_include1, blank_count1 = check_discountinuous_line_recursion(point, direction[0])
+        count2, isOpen2, is_blank_include2, blank_count2 = check_discountinuous_line_recursion(point, direction[1], is_blank_include1)
         count = count1 + count2 + 1
         isOpen = isOpen1 and isOpen2
 
@@ -130,18 +131,21 @@ def check_33_rules(originPoint, point):
             break
     return
 
-def check_44_rules(originPoint, point):
+def check_44_rule(originPoint, point):
     global unselectablePointList
 
     row, col = point
-    directionList = [((1, 0), (-1, 0)), ((0, 1), (0, -1)), ((1, 1), (-1, -1)), ((1, -1), (-1, 1))]
+    directionTupleList = [((1, 0), (-1, 0)), ((0, 1), (0, -1)), ((1, 1), (-1, -1)), ((1, -1), (-1, 1))]
     
     lineCount = 0
-    for direction in directionList:
-        count1, isOpen1, is_blank_include1 = check_discountinuous_line_recursion(point, direction[0])
-        count2, isOpen2, is_blank_include2 = check_discountinuous_line_recursion(point, direction[1], is_blank_include1)
+    for direction in directionTupleList:
+        count1, isOpen1, is_blank_include1, blank_count1 = check_discountinuous_line_recursion(point, direction[0])
+        count2, isOpen2, is_blank_include2, blank_count2 = check_discountinuous_line_recursion(point, direction[1], is_blank_include1)
         count = count1 + count2 + 1
         isOpen = isOpen1 or isOpen2
+        
+        if isOpen == False and blank_count1 + blank_count2 > 1:
+            isOpen = True
 
         if (count == 4 and isOpen):
             lineCount += 1
@@ -159,20 +163,20 @@ def check_discountinuous_line_recursion(point, direction, is_include_blank = Fal
     row, col = point
     
     if blank_count >= 2:
-        return (0, True, is_include_blank)
+        return (0, True, is_include_blank, blank_count)
     elif is_out_of_array(point) or array[row][col] == 3:
-        return (0, False, is_include_blank)
+        return (0, False, is_include_blank, blank_count)
     elif array[row][col] == 0 or array[row][col] == 1:
         if is_include_blank:
-            return (0, True, is_include_blank)
+            return (0, True, is_include_blank, blank_count + 1)
         else:
-            _count, _isOpen, _is_include_blank = check_discountinuous_line_recursion(point, direction, is_include_blank, blank_count + 1)
-            return (_count, True and _isOpen, _is_include_blank)
+            _count, _isOpen, _is_include_blank, _blank_count = check_discountinuous_line_recursion(point, direction, is_include_blank, blank_count + 1)
+            return (_count, True and _isOpen, _is_include_blank, _blank_count)
     else:
         if blank_count > 0:
             is_include_blank = True
-        _count, _isOpen, _is_include_blank = check_discountinuous_line_recursion(point, direction, is_include_blank, blank_count)
-        return (_count + 1, True and _isOpen, _is_include_blank)
+        _count, _isOpen, _is_include_blank, _blank_count = check_discountinuous_line_recursion(point, direction, is_include_blank, blank_count)
+        return (_count + 1, True and _isOpen, _is_include_blank, _blank_count)
 
 def is_finished_game(leftSelectableCount, point, isWhiteTurn):
     # Need to Make Enum
@@ -202,6 +206,24 @@ def check_lines(point, isWhiteTurn):
 
     return False
 
+def check_over_5_rule(point):
+    row, col = point
+    directionTupleList = [((1, 0), (-1, 0)), ((0, 1), (0, -1)), ((1, 1), (-1, -1)), ((1, -1), (-1, 1))]
+    for direction in directionTupleList:
+        userIndex = isWhiteTurn + 2
+        count1 = check_continuous_line_recursion(point, direction[0], userIndex)
+        count2 = check_continuous_line_recursion(point, direction[1], userIndex)
+        count = count1 + count2 + 1
+
+
+        if count > 5:
+            print("Unselectable!" + str(point))
+            array[row][col] = 1
+            unselectablePointList.append(point)
+            break
+    return
+
+
 def check_continuous_line_recursion(point, direction, userIndex):
     point = (point[0] + direction[0], point[1] + direction[1])
     row, col = point
@@ -228,18 +250,29 @@ testBlackPreSettingList = [
     # (3, 5), 
     # (3, 6),
     # (7, 6),
+
+    # (6, 6),
+    # (6, 7),
+    # (6, 9),
+    # (5, 8),
+    # (8, 8),
 ]
 
 testWhitePreSettingList = [
     # (2, 6),
     # (8, 6)
+
+    # (3, 8),
+    # (10, 8),
 ]
 
 testTurnList = [
-    (3, 3), 
-    (3, 4),
-    (3, 5),
-    (5, 5),
+    # (3, 3), 
+    # (3, 4),
+    # (3, 5),
+    # (5, 5),
+
+    # (7, 8),
 ]
 
 for row, col in testBlackPreSettingList:
@@ -289,10 +322,11 @@ while gameState == 0:
         unselectablePointList.remove((row, col))
 
     userIndex = isWhiteTurn + 2
+    print("userIndex: " + str(userIndex))
     array[row][col] = userIndex
-    
+
     if isWhiteTurn == False:
-        detect_unselectable_points((row, col))
+        detect_unselectable_points()
     else:
         pass
 
