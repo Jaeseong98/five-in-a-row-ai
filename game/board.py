86f93f4ca63129a9e4fce9164af27ec3b036e09d
Jaeseong98 <gossip6017@gamil.com>
2020-12-15 22:24:12 +0900

    [game] Add rule for checking unselectable point

--- game/board.py
+++ game/board.py
@@ -23,6 +23,9 @@ class GameBoard(object):
         self.turn = TurnStateEnum.BLACK
         self.black_agent = black_agent
         self.white_agent = white_agent
+        self.directions = [
+            (i, j) for i in range(-1, 2, 1) for j in range(-1, 2, 1) if i or j
+        ]
 
         try:
             mode_function_map = {
@@ -62,15 +65,6 @@ class GameBoard(object):
             ]) for line in self.array
         ])
 
-    # @property
-    # def unselectable_points(self):
-    #     unselectable_points = list()
-    #     for row, line in enumerate(self.array):
-    #         for col, point in enumerate(line):
-    #             if point == PointStateEnum.UNSELECTABLE:
-    #                 unselectable_points.append((row, col))
-    #     return unselectable_points
-
     def change_turn(self):
         if self.turn == TurnStateEnum.BLACK:
             self.turn = TurnStateEnum.WHITE
@@ -88,6 +82,10 @@ class GameBoard(object):
         row, col = point
         return row < 0 or BOARD_SIZE - 1 < row or col < 0 or BOARD_SIZE - 1 < col
 
+    def set_point_state(self, point, state):
+        row, col = point
+        self.array[row][col] = state
+
     def start(self):
         point_states = [PointStateEnum.BLACK, PointStateEnum.WHITE]
 
@@ -95,36 +93,37 @@ class GameBoard(object):
             try:
                 for move_function in self.move_functions:
                     row, col = move_function()
+                    point = (row, col)
                     self.array[row][col] = self.get_current_turn_point_state()
+
+                    # self.totalBlankCount -= 1
+                    # self.check_finished(
+                    #     self.totalBlankCount - len(self.unselectable_points),
+                    #     point
+                    # )
+
                     if self.turn == TurnStateEnum.BLACK:
-                        self.detect_unselectable_points()
+                        self.set_unselectable_points(point)
                     else:
                         if self.array[row][col] == PointStateEnum.UNSELECTABLE:
-                            self.unselectable_points.remove((row, col))
-                    self.detect_selectable_points()
+                            self.unselectable_points.remove(point)
+                        self.set_selectable_points()
                     self.change_turn()
-                    self.totalBlankCount -= 1
-                    self.check_finished(
-                        self.totalBlankCount - len(self.unselectable_points),
-                        (row, col)
-                    )
 
             except KeyboardInterrupt:
-                print("Stop Game")
+                print("Stop Game by keyboard interrupt")
                 break
             except TestEndError:
                 break
-            # except Exception:
-            #     print(traceback.format_exc())
 
     def check_finished(self, left, point):
         # Need to Make Enum
         # 0: No Win, 1: Black Win, 2: White Win, 3: Draw
 
-        if left == 0:
-            return GamestateEnum.DRAW
-        elif self.check_lines(point):
+        if self.check_lines(point):
             return GamestateEnum.WHITE if self.turn == TurnStateEnum.WHITE else GamestateEnum.BLACK  # Ternary Operator
+        elif left == 0:
+            return GamestateEnum.DRAW
         else:
             return GamestateEnum.CONTINUE
 
@@ -359,3 +358,170 @@ class GameBoard(object):
             return 0
         else:
             return 1 + self.check_continuous_line_recursion(point, direction, userIndex)
+
+    def set_unselectable_points(self, point):
+        # Overline
+        for direction in self.directions:
+            forward_count, _, _, empty_point, _ = self.check_line_state(
+                point, direction, PointStateEnum.BLACK, True)
+            backward_count, _, _, _, _ = self.check_line_state(
+                point, (-direction[0], -direction[1]), PointStateEnum.BLACK)
+            count = forward_count + backward_count + 1
+            if count >= 5:
+                self.set_point_state(empty_point, PointStateEnum.UNSELECTABLE)
+                self.unselectable_points.append(empty_point)
+        # double_three & double_four
+        for origin_direction in self.directions:
+            for i in range(1, 5):
+                row = point[0] + i * origin_direction[0]
+                col = point[1] + i * origin_direction[1]
+                search_point = (row, col)
+                if (
+                    self.is_out_of_array(search_point)
+                    or self.array[row][col] != PointStateEnum.EMPTY
+                ):
+                    continue
+                three, four = False, False
+                checked_line_points = list()
+                for direction in self.directions:
+                    forward_count, forward_emtpy_count, is_countious, _, forward_point = self.check_line_state(
+                        search_point, direction, PointStateEnum.BLACK, True)
+                    backward_count, backward_emtpy_count, _, _, backward_point = self.check_line_state(
+                        search_point, (-direction[0], -direction[1]), PointStateEnum.BLACK)
+                    stone_count = forward_count + backward_count + 1
+                    possible_count = (
+                        stone_count + forward_emtpy_count + backward_emtpy_count + int(not is_countious)
+                    )
+                    if possible_count < 5 or (backward_point, forward_point) in checked_line_points:
+                        continue
+                    # double three
+                    if (
+                        stone_count == 3
+                        and forward_emtpy_count
+                        and backward_emtpy_count
+                        and possible_count > 5
+                    ):
+                        # if search_point == (6, 4):
+                        #     print(point, direction)
+                        #     print(forward_count, forward_emtpy_count, is_countious)
+                        #     print(backward_count, backward_emtpy_count)
+                        #     print(backward_point, forward_point)
+                        #     print('')
+                        if three:
+                            self.set_point_state(search_point, PointStateEnum.UNSELECTABLE)
+                            self.unselectable_points.append(search_point)
+                            break
+                        three = True
+                    # double four
+                    elif stone_count == 4:
+                        if four:
+                            self.set_point_state(search_point, PointStateEnum.UNSELECTABLE)
+                            self.unselectable_points.append(search_point)
+                            break
+                        four = True
+                    checked_line_points.append((forward_point, backward_point))
+
+    def set_selectable_points(self):
+        for point in self.unselectable_points:
+            three, double_three = False, False
+            four, double_four = False, False
+            overline, endable = False, False
+            checked_line_points = list()
+            for direction in self.directions:
+                forward_count, forward_emtpy_count, is_countious, _, forward_point = self.check_line_state(
+                    point, direction, PointStateEnum.BLACK, True)
+                backward_count, backward_emtpy_count, _, _, backward_point = self.check_line_state(
+                    point, (-direction[0], -direction[1]), PointStateEnum.BLACK)
+                stone_count = forward_count + backward_count + 1
+                possible_count = (
+                    stone_count + forward_emtpy_count + backward_emtpy_count + int(not is_countious)
+                )
+                if possible_count < 5 or (backward_point, forward_point) in checked_line_points:
+                    continue
+                if self.check_five(point, PointStateEnum.BLACK):
+                    endable = True
+                    break
+                # double three
+                if (
+                    stone_count == 3
+                    and forward_emtpy_count
+                    and backward_emtpy_count
+                    and possible_count > 5
+                ):
+                    # print('unset')
+                    # print(point, direction)
+                    # print(forward_count, forward_emtpy_count, is_countious)
+                    # print(backward_count, backward_emtpy_count)
+                    # print(backward_point, forward_point)
+                    # print('')
+                    if three:
+                        double_three = True
+                    three = True
+                # double four
+                elif stone_count == 4:
+                    if four:
+                        double_four = True
+                        break
+                    four = True
+                elif stone_count >= 5:
+                    overline = True
+                checked_line_points.append((forward_point, backward_point))
+            # if point == (6, 4):
+            #     print(endable, double_three, double_four, overline)
+            if endable or not (double_three or double_four or overline):
+                self.set_point_state(point, PointStateEnum.EMPTY)
+                self.unselectable_points.remove(point)
+
+    def check_line_state(self, point, direction, state, allow_empty=False):
+        row_dir, col_dir = direction
+        row, col = point
+        opposite_state = state.get_opposite()
+        stone_count, empty_count = 0, 0
+        empty_point = tuple()
+        is_continous = True
+        while True:
+            row, col = row + row_dir, col + col_dir
+            if self.is_out_of_array((row, col)) or self.array[row][col] == opposite_state:
+                break
+            elif self.array[row][col] in [
+                PointStateEnum.EMPTY,
+                PointStateEnum.UNSELECTABLE
+            ]:
+                empty_count += 1
+                if empty_count >= 3:
+                    break
+            elif self.array[row][col] == state:
+                if state == PointStateEnum.BLACK:
+                    if empty_count:
+                        if allow_empty and is_continous and empty_count == 1:
+                            is_continous = False
+                            stone_count += 1
+                            empty_count = 0
+                            empty_point = (row - row_dir, col - col_dir)
+                            continue
+                        else:
+                            empty_count -= 1
+                            row, col = row - row_dir, col - col_dir
+                            break
+                    else:
+                        stone_count += 1
+                else:
+                    if empty_count:
+                        break
+                    stone_count += 1
+        return stone_count, empty_count, is_continous, empty_point, (row, col)
+
+    def check_five(self, point, color):
+        for direction in self.directions:
+            forward_count, _, _, _, _ = self.check_line_state(
+                point, direction, color)
+            backward_count, _, _, _, _ = self.check_line_state(
+                point, (-direction[0], -direction[1]), color)
+            count = forward_count + backward_count + 1
+
+            if color == PointStateEnum.BLACK and count == 5:
+                return True
+            if color == PointStateEnum.WHITE and count >= 5:
+                return True
+
+        return False
