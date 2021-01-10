cd283768052ba1d209dfbf1c383d248570a27295
Jaeseong98 <gossip6017@gmail.com>
2021-01-11 01:25:24 +0900

    [agent] Add turn to agent

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,8 +1,13 @@
+from game.enum import TurnStateEnum
+
 class BaseAgent(object):
     """
     Base class for Agent
 
     """
+    def __init__(self, turn: TurnStateEnum):
+        self.turn = turn
+
     def get_next_point(self):
         raise NotImplementedError(
             "get_next_point() has to be implemented by subclasses"
