77944d6e4145e34de8bed08a11091a8e75290cc2
minsang <minsang5749@naver.com>
2021-01-10 13:35:28 +0900

    [PG] Modify config and then adjust it in pygame code

--- pygame_FIR.py
+++ pygame_FIR.py
@@ -1,97 +1,71 @@
 from game.enum import PygameStateEnum
-from game.config import PygameColor
+from game.config import PygameConfig
 
 import pygame
 import math
 
 pygame.init()
 
-PygameColor.COLOR_RED = (255, 0, 0)
-PygameColor.COLOR_BLACK = (0, 0, 0)
-PygameColor.COLOR_BOARD = (240, 220, 165)
-PygameColor.COLOR_GRAY = (180, 180, 180)
-PygameColor.COLOR_UNSELECTABLE_BG = (255, 175, 175)
-PygameColor.COLOR_WHITE = (255, 255, 255)
-PygameColor.COLOR_YELLOW = (255, 255, 0)
-
-cell_count = 15
-cell_size = 30
-
-radius = 13
-diff = 7
-
-board_start_w = 40
-board_start_h = 40
-board_end_w = board_start_w + cell_size * (cell_count - 1)
-board_end_h = board_start_h + cell_size * (cell_count - 1)
-
-btn_bg_w = 280
-btn_bg_h = 50
-btn_bg_start_w = 125
-btn_bg_start_h = 200
-
-size = [500, 500]
-
 def _GetPygamePos(pos):
     i, j = pos
-    return (board_start_w + i * cell_size, board_start_h + j * cell_size)
+    return (PygameConfig.BOARD_START_W + i * PygameConfig.CELL_SIZE, PygameConfig.BOARD_START_H + j * PygameConfig.CELL_SIZE)
 
 def _DrawHoverZone(pos):
-    pygame.draw.circle(screen, PygameColor.COLOR_YELLOW, _GetPygamePos(pos), radius - 10, width = 0)
+    pygame.draw.circle(screen, PygameConfig.COLOR_YELLOW, _GetPygamePos(pos), PygameConfig.STONE_RADIUS - 10, width = 0)
     return
 
 def _EraseHoverZone(pos):
-    pygame.draw.circle(screen, PygameColor.COLOR_BOARD, _GetPygamePos(pos), radius - 10, width = 0)
+    pygame.draw.circle(screen, PygameConfig.COLOR_BOARD, _GetPygamePos(pos), PygameConfig.STONE_RADIUS - 10, width = 0)
     
     w, h = pos
-    w_line = board_start_w + cell_size * w
-    h_line = board_start_h + cell_size * h
+    w_line = PygameConfig.BOARD_START_W + PygameConfig.CELL_SIZE * w
+    h_line = PygameConfig.BOARD_START_H + PygameConfig.CELL_SIZE * h
     w_start = 0.5 if w > 0 else 0
     w_end = 0.5 if w < 14 else 0
     h_start = 0.5 if h > 0 else 0
     h_end = 0.5 if h < 14 else 0
-    pygame.draw.line(screen, PygameColor.COLOR_BLACK, (w_line - w_start * cell_size, h_line), (w_line + w_end * cell_size , h_line), width = 2)
-    pygame.draw.line(screen, PygameColor.COLOR_BLACK, (w_line, h_line - h_start * cell_size), (w_line, h_line + h_end * cell_size), width = 2)
+    pygame.draw.line(screen, PygameConfig.COLOR_BLACK, (w_line - w_start * PygameConfig.CELL_SIZE, h_line), (w_line + w_end * PygameConfig.CELL_SIZE , h_line), width = 2)
+    pygame.draw.line(screen, PygameConfig.COLOR_BLACK, (w_line, h_line - h_start * PygameConfig.CELL_SIZE), (w_line, h_line + h_end * PygameConfig.CELL_SIZE), width = 2)
     return
 
 def _DrawBlackStone(pos):
-    pygame.draw.circle(screen, PygameColor.COLOR_BLACK, _GetPygamePos(pos), radius, width = 0)
+    pygame.draw.circle(screen, PygameConfig.COLOR_BLACK, _GetPygamePos(pos), PygameConfig.STONE_RADIUS, width = 0)
     return
 
 def _DrawWhiteStone(pos):
-    pygame.draw.circle(screen, PygameColor.COLOR_WHITE, _GetPygamePos(pos), radius, width = 0)
-    pygame.draw.circle(screen, PygameColor.COLOR_BLACK, _GetPygamePos(pos), radius, width = 1)
+    pygame.draw.circle(screen, PygameConfig.COLOR_WHITE, _GetPygamePos(pos), PygameConfig.STONE_RADIUS, width = 0)
+    pygame.draw.circle(screen, PygameConfig.COLOR_BLACK, _GetPygamePos(pos), PygameConfig.STONE_RADIUS, width = 1)
     return
 
 def _DrawUnseletable(pos):
     pg_pos = _GetPygamePos(pos)
-    pygame.draw.circle(screen, PygameColor.COLOR_UNSELECTABLE_BG, pg_pos, radius, width = 0)
-    pygame.draw.circle(screen, PygameColor.COLOR_RED, pg_pos, radius, width = 2)
+    pygame.draw.circle(screen, PygameConfig.COLOR_UNSELECTABLE_BG, pg_pos, PygameConfig.STONE_RADIUS, width = 0)
+    pygame.draw.circle(screen, PygameConfig.COLOR_RED, pg_pos, PygameConfig.STONE_RADIUS, width = 2)
 
     pg_pos_w, pg_pos_h = pg_pos
-    pygame.draw.line(screen, PygameColor.COLOR_RED, (pg_pos_w - diff, pg_pos_h - diff), (pg_pos_w + diff, pg_pos_h + diff), width = 3)
-    pygame.draw.line(screen, PygameColor.COLOR_RED, (pg_pos_w + diff, pg_pos_h - diff), (pg_pos_w - diff, pg_pos_h + diff), width = 3)
+    pygame.draw.line(screen, PygameConfig.COLOR_RED, (pg_pos_w - PygameConfig.STONE_DIFF, pg_pos_h - PygameConfig.STONE_DIFF), (pg_pos_w + PygameConfig.STONE_DIFF, pg_pos_h + PygameConfig.STONE_DIFF), width = 3)
+    pygame.draw.line(screen, PygameConfig.COLOR_RED, (pg_pos_w + PygameConfig.STONE_DIFF, pg_pos_h - PygameConfig.STONE_DIFF), (pg_pos_w - PygameConfig.STONE_DIFF, pg_pos_h + PygameConfig.STONE_DIFF), width = 3)
     return
 
 def _IsButtonCollision(pos):
     for i in range(3):
         w, h = pos
-        cur_btn_bg_start_w = btn_bg_start_w
-        cur_btn_bg_start_h = btn_bg_start_h + (3 / 2 * btn_bg_h * i)
-        if (cur_btn_bg_start_w <= w and w <= cur_btn_bg_start_w + btn_bg_w) and (cur_btn_bg_start_h <= h and h <= cur_btn_bg_start_h + btn_bg_h):
+        cur_btn_bg_start_w = PygameConfig.BUTTON_START_W
+        cur_btn_bg_start_h = PygameConfig.BUTTON_START_H + (3 / 2 * PygameConfig.BUTTON_H * i)
+        if (cur_btn_bg_start_w <= w and w <= cur_btn_bg_start_w + PygameConfig.BUTTON_W) and (cur_btn_bg_start_h <= h and h <= cur_btn_bg_start_h + PygameConfig.BUTTON_H):
             return i
     return -1
 
 def _IsCellCollision(pos):
     w, h = pos
-    div_w, mod_w = divmod((w - board_start_w + 0.5 * cell_size), cell_size)
-    div_h, mod_h = divmod((h - board_start_h + 0.5 * cell_size), cell_size)
+    div_w, mod_w = divmod((w - PygameConfig.BOARD_START_W + 0.5 * PygameConfig.CELL_SIZE), PygameConfig.CELL_SIZE)
+    div_h, mod_h = divmod((h - PygameConfig.BOARD_START_H + 0.5 * PygameConfig.CELL_SIZE), PygameConfig.CELL_SIZE)
 
     if ((3 < mod_w and mod_w < 27) and (3 < mod_h and mod_h < 27)):
         return (int(div_w), int(div_h))
     return (-1, -1)
 
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(PygameConfig.SCREEN_SIZE)
 
 title_name = "Five In A Row"
 pygame.display.set_caption(title_name)
@@ -107,7 +81,7 @@ cur_hover_w, cur_hover_h = -1, -1
 hover_w, hover_h = -1, -1
 click_w, click_h = -1, -1
 
-array = [ [ 0 for i in range(cell_count) ] for j in range(cell_count)]
+array = [ [ 0 for i in range(PygameConfig.CELL_COUNT) ] for j in range(PygameConfig.CELL_COUNT)]
 
 is_black_turn = True
 
@@ -139,10 +113,10 @@ while not done:
             buttonClickIndex = -1
             isButtonHoverList = []
 
-            screen.fill(PygameColor.COLOR_BOARD)
+            screen.fill(PygameConfig.COLOR_BOARD)
 
             fontObj = pygame.font.Font(None, 60)
-            textObj = fontObj.render('Five in a Row', True, PygameColor.COLOR_BLACK)
+            textObj = fontObj.render('Five in a Row', True, PygameConfig.COLOR_BLACK)
             screen.blit(textObj, (135, 70))
 
 
@@ -150,15 +124,15 @@ while not done:
             strList = ['Human vs Human', 'Human vs AI(Rule-Base)', 'Human vs AI(RL)']
             buttonPosList = []
             for i in range(3):
-                cur_btn_bg_start_w = btn_bg_start_w
-                cur_btn_bg_start_h = btn_bg_start_h + (3 / 2 * btn_bg_h * i)
+                cur_btn_bg_start_w = PygameConfig.BUTTON_START_W
+                cur_btn_bg_start_h = PygameConfig.BUTTON_START_H + (3 / 2 * PygameConfig.BUTTON_H * i)
 
-                textBG = pygame.draw.rect(screen, PygameColor.COLOR_GRAY, (cur_btn_bg_start_w, cur_btn_bg_start_h, btn_bg_w, btn_bg_h), width=0)
+                textBG = pygame.draw.rect(screen, PygameConfig.COLOR_GRAY, (cur_btn_bg_start_w, cur_btn_bg_start_h, PygameConfig.BUTTON_W, PygameConfig.BUTTON_H), width=0)
                 
-                background_center_w = cur_btn_bg_start_w + btn_bg_w / 2
-                background_center_h = cur_btn_bg_start_h + btn_bg_h / 2
+                background_center_w = cur_btn_bg_start_w + PygameConfig.BUTTON_W / 2
+                background_center_h = cur_btn_bg_start_h + PygameConfig.BUTTON_H / 2
 
-                textObj = fontObj.render(strList[i], True, PygameColor.COLOR_BLACK)
+                textObj = fontObj.render(strList[i], True, PygameConfig.COLOR_BLACK)
                 (x, y, w, h) = textObj.get_rect()
                 text_x = background_center_w - w / 2
                 text_y = background_center_h - h / 2
@@ -171,39 +145,39 @@ while not done:
         buttonUpdateList = []
         for i in range(3):
             if buttonHoverIndex != i and isButtonHoverList[i] == True:
-                cur_btn_bg_start_w = btn_bg_start_w
-                cur_btn_bg_start_h = btn_bg_start_h + (3 / 2 * btn_bg_h * i)
+                cur_btn_bg_start_w = PygameConfig.BUTTON_START_W
+                cur_btn_bg_start_h = PygameConfig.BUTTON_START_H + (3 / 2 * PygameConfig.BUTTON_H * i)
 
-                textBG = pygame.draw.rect(screen, PygameColor.COLOR_GRAY, (cur_btn_bg_start_w, cur_btn_bg_start_h, btn_bg_w, btn_bg_h), width=0)
+                textBG = pygame.draw.rect(screen, PygameConfig.COLOR_GRAY, (cur_btn_bg_start_w, cur_btn_bg_start_h, PygameConfig.BUTTON_W, PygameConfig.BUTTON_H), width=0)
                 
-                background_center_w = cur_btn_bg_start_w + btn_bg_w / 2
-                background_center_h = cur_btn_bg_start_h + btn_bg_h / 2
+                background_center_w = cur_btn_bg_start_w + PygameConfig.BUTTON_W / 2
+                background_center_h = cur_btn_bg_start_h + PygameConfig.BUTTON_H / 2
 
-                textObj = fontObj.render(strList[i], True, PygameColor.COLOR_BLACK)
+                textObj = fontObj.render(strList[i], True, PygameConfig.COLOR_BLACK)
                 (x, y, w, h) = textObj.get_rect()
                 text_x = background_center_w - w / 2
                 text_y = background_center_h - h / 2
                 screen.blit(textObj, (text_x, text_y))
 
-                buttonUpdateList.append((cur_btn_bg_start_w, cur_btn_bg_start_h, btn_bg_w, btn_bg_h))
+                buttonUpdateList.append((cur_btn_bg_start_w, cur_btn_bg_start_h, PygameConfig.BUTTON_W, PygameConfig.BUTTON_H))
                 isButtonHoverList[i] = False
 
             if buttonHoverIndex == i and isButtonHoverList[i] == False:
-                cur_btn_bg_start_w = btn_bg_start_w
-                cur_btn_bg_start_h = btn_bg_start_h + (3 / 2 * btn_bg_h * i)
+                cur_btn_bg_start_w = PygameConfig.BUTTON_START_W
+                cur_btn_bg_start_h = PygameConfig.BUTTON_START_H + (3 / 2 * PygameConfig.BUTTON_H * i)
 
-                textBG = pygame.draw.rect(screen, PygameColor.COLOR_GRAY, (cur_btn_bg_start_w, cur_btn_bg_start_h, btn_bg_w, btn_bg_h), width=0)
+                textBG = pygame.draw.rect(screen, PygameConfig.COLOR_GRAY, (cur_btn_bg_start_w, cur_btn_bg_start_h, PygameConfig.BUTTON_W, PygameConfig.BUTTON_H), width=0)
                 
-                background_center_w = cur_btn_bg_start_w + btn_bg_w / 2
-                background_center_h = cur_btn_bg_start_h + btn_bg_h / 2
+                background_center_w = cur_btn_bg_start_w + PygameConfig.BUTTON_W / 2
+                background_center_h = cur_btn_bg_start_h + PygameConfig.BUTTON_H / 2
 
-                textObj = fontObj.render(strList[i], True, PygameColor.COLOR_WHITE)
+                textObj = fontObj.render(strList[i], True, PygameConfig.COLOR_WHITE)
                 (x, y, w, h) = textObj.get_rect()
                 text_x = background_center_w - w / 2
                 text_y = background_center_h - h / 2
                 screen.blit(textObj, (text_x, text_y))
 
-                buttonUpdateList.append((cur_btn_bg_start_w, cur_btn_bg_start_h, btn_bg_w, btn_bg_h))
+                buttonUpdateList.append((cur_btn_bg_start_w, cur_btn_bg_start_h, PygameConfig.BUTTON_W, PygameConfig.BUTTON_H))
                 isButtonHoverList[i] = True
 
         if len(buttonUpdateList) > 0:
@@ -224,11 +198,11 @@ while not done:
 
         if is_first_draw_game:
             is_first_draw_game = False
-            screen.fill(PygameColor.COLOR_BOARD)
+            screen.fill(PygameConfig.COLOR_BOARD)
 
-            for i in range(cell_count):
-                pygame.draw.line(screen, PygameColor.COLOR_BLACK, (board_start_w + cell_size * i , board_start_h), (board_start_w + cell_size * i , board_end_h), width = 2)
-                pygame.draw.line(screen, PygameColor.COLOR_BLACK, (board_start_w, board_start_h + cell_size * i), (board_end_w, board_start_h + cell_size * i), width = 2)
+            for i in range(PygameConfig.CELL_COUNT):
+                pygame.draw.line(screen, PygameConfig.COLOR_BLACK, (PygameConfig.BOARD_START_W + PygameConfig.CELL_SIZE * i , PygameConfig.BOARD_START_H), (PygameConfig.BOARD_START_W + PygameConfig.CELL_SIZE * i , PygameConfig.BOARD_END_H), width = 2)
+                pygame.draw.line(screen, PygameConfig.COLOR_BLACK, (PygameConfig.BOARD_START_W, PygameConfig.BOARD_START_H + PygameConfig.CELL_SIZE * i), (PygameConfig.BOARD_END_W, PygameConfig.BOARD_START_H + PygameConfig.CELL_SIZE * i), width = 2)
 
             _DrawUnseletable((5, 7))
             array[5][7] = 1
@@ -240,11 +214,11 @@ while not done:
             if array[hover_w][hover_h] == 0:
                 if((0 <= cur_hover_w and cur_hover_w <= 14) and (0 <= cur_hover_h and cur_hover_h <= 14)):
                     _EraseHoverZone((cur_hover_w, cur_hover_h))
-                    updateRectList.append(pygame.Rect(board_start_w + cell_size * (cur_hover_w - 0.5), board_start_h + cell_size * (cur_hover_h - 0.5), cell_size, cell_size))
+                    updateRectList.append(pygame.Rect(PygameConfig.BOARD_START_W + PygameConfig.CELL_SIZE * (cur_hover_w - 0.5), PygameConfig.BOARD_START_H + PygameConfig.CELL_SIZE * (cur_hover_h - 0.5), PygameConfig.CELL_SIZE, PygameConfig.CELL_SIZE))
 
                 cur_hover_w, cur_hover_h = hover_w, hover_h
                 _DrawHoverZone((cur_hover_w, cur_hover_h))
-                updateRectList.append(pygame.Rect(board_start_w + cell_size * (cur_hover_w - 0.5), board_start_h + cell_size * (cur_hover_h - 0.5), cell_size, cell_size))
+                updateRectList.append(pygame.Rect(PygameConfig.BOARD_START_W + PygameConfig.CELL_SIZE * (cur_hover_w - 0.5), PygameConfig.BOARD_START_H + PygameConfig.CELL_SIZE * (cur_hover_h - 0.5), PygameConfig.CELL_SIZE, PygameConfig.CELL_SIZE))
 
         if ((0 <= click_w and click_w <= 14) and (0 <= click_h and click_h <= 14)):
             if array[click_w][click_h] == 0:
@@ -259,7 +233,7 @@ while not done:
 
                 is_black_turn = not is_black_turn
 
-                updateRectList.append(pygame.Rect(board_start_w + cell_size * (click_w - 0.5), board_start_h + cell_size * (click_h - 0.5), cell_size, cell_size))
+                updateRectList.append(pygame.Rect(PygameConfig.BOARD_START_W + PygameConfig.CELL_SIZE * (click_w - 0.5), PygameConfig.BOARD_START_H + PygameConfig.CELL_SIZE * (click_h - 0.5), PygameConfig.CELL_SIZE, PygameConfig.CELL_SIZE))
 
 
         if len(updateRectList) > 0:
